3c202ab024af478b5f95b63a3e55952afa80706f
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-07-05 16:27:31 +0700

    Animate element out of DOM with Animate Presence

--- lesson-2/pizzajoint/src/components/Order.js
+++ lesson-2/pizzajoint/src/components/Order.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      // delay: 0.2,
+      mass: 0.4, // adjust for delay but faster
+      damping: 8,  // adjust for spring but faster
+      stiffness: 120,
+      when: 'beforeChildren', // parent run first
+      staggerChildren: 0.4 // children copy parent variant and set delay 0.4
+    }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  }
+}
 
 const Order = ({ pizza }) => {
-
-  const containerVariants = {
-    hidden: {
-      opacity: 0,
-      x: '100vw'
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        type: 'spring',
-        // delay: 0.2,
-        mass: 0.4, // adjust for delay but faster
-        damping: 8,  // adjust for spring but faster
-        stiffness: 120,
-        when: 'beforeChildren', // parent run first
-        staggerChildren: 0.4 // children copy parent variant and set delay 0.4
-      }
-    }
-  }
-
-  const childVariants = {
-    hidden: {
-      opacity: 0
-    },
-    visible: {
-      opacity: 1
-    }
-  }
-
+  const [showTitle, setShowTitle] = useState(true)
+  setTimeout(() => {
+    setShowTitle(false)
+  }, 4000)
   return (
     <motion.div className="container order"
       variants={containerVariants}
       initial='hidden'
       animate='visible'
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && <motion.h2 exit={{y: -1000}}
+        >Thank you for your order :)</motion.h2> }
+      </AnimatePresence>
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
